bae3502ecb13f0e4237cfd1a5a6f43cbd59edb2f
veerasolaiyappan <newlinedeveloper@gmail.com>
2020-04-08 16:05:48 +0530

    Modify listing and realtors files

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
+from .models import Listing
 
 
 def  index(requests):
-	return render(requests,'listings/listings.html')
+
+	listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+	paginator = Paginator(listings,6)
+	page = requests.GET.get('page')
+	paged_listings = paginator.get_page(page)
 
 
-def  listing(requests):
+	context = {
+	'listings' : paged_listings
+	} 
+
+	return render(requests,'listings/listings.html',context)
+
+
+def  listing(requests,listing_id):
 	return render(requests,'listings/listing.html')
 
 
--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,31 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+from listings.models import Listing
+from realtors.models import Realtor 
+
+
 def index(requests):
-	return render(requests,"pages/index.html")
+	listings = Listing.objects.order_by('-list_date').filter(is_published = True)[:3]
+
+	context = {
+
+	'listings' : listings
+
+	}
+	return render(requests,"pages/index.html",context)
 
 def about(requests):
-	return render(requests,"pages/about.html")
+	#Get all realtors
+	realtors = Realtor.objects.order_by('-hire_date')
+
+	#Get MVP
+	mvp_realtors = Realtor.objects.all().filter(is_mvp = True)
+
+	context = {
+
+	'realtors' :realtors,
+	'mvp_realtors': mvp_realtors
+	}
+
+	return render(requests,"pages/about.html",context)
