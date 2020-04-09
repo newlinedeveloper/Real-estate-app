934a63b5e7d7f37b1eba4e1309f111ed613d8054
veerasolaiyappan <newlinedeveloper@gmail.com>
2020-04-09 19:06:23 +0530

    third commit

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
@@ -22,8 +22,29 @@ def  index(requests):
 
 
 def  listing(requests,listing_id):
-	return render(requests,'listings/listing.html')
+
+	listing = get_object_or_404(Listing,pk=listing_id)
+
+	context = {
+
+		'listing' : listing
+	}
+	return render(requests,'listings/listing.html',context)
 
 
 def  search(requests):
-	return render(requests,'listings/listing.html')
+
+	queryset_list = Listing.objects.order_by('-list_date')
+
+
+	if 'keywords' in requests.GET:
+		keywords = request.GET('keywords')
+
+	if keywords:
+		queryset_list = queryset_list.filter(description_icontains=keywords)
+
+	context ={
+		'listings' : queryset_list
+	}
+
+	return render(requests,'listings/listing.html',context)	
