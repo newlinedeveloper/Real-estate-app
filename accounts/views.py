1db3c46d272a55166cd35a7f58ef44bbaa65c3bb
veerasolaiyappan <newlinedeveloper@gmail.com>
2020-04-10 13:01:07 +0530

    51. User Registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
 
@@ -7,6 +8,8 @@ def login(requests):
 
 	if requests.method == 'POST':
 		# print('SUBMITTED REG')
+
+
 		messages.error(requests,'Testing error message')
 		return redirect('login')
 	else:
@@ -21,6 +24,32 @@ def logout(requests):
 def register(requests):
 
 	if requests.method == 'POST':
+
+		first_name = requests.POST['first_name']
+		last_name = requests.POST['last_name']
+		username = requests.POST['username']
+		email = requests.POST['email']
+		password = requests.POST['password']
+		password2 = requests.POST['password2']
+		
+		# check passwords
+		if password == password2:
+			# check username
+			if User.objects.filter(username=username).exists():
+				messages.error(requests,'That username is taked')
+				return redirect('register')
+			else:
+				if User.objects.filter(email = email).exists():
+					messages.error(requests,'That email is being used')
+					return redirect('register')
+				else:
+					 user = User.objects.create_user(username = username,password = password,email=email,first_name=first_name,last_name =last_name)
+					 auth.login(requests,user)
+					 messages.success(requests,'You are now logged in')
+					 return redirect('login')
+		else:
+			messages.error(requests,"Passwords are not match")
+
 		# print('SUBMITTED REG')
 		messages.error(requests,'Testing error message')
 		return redirect('register')
