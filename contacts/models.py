35b946591b1681d83f53af2423e0afbb99bb3c1d
veerasolaiyappan <newlinedeveloper@gmail.com>
2020-04-10 16:40:44 +0530

    58. Contact Form Submission

--- contacts/models.py
+++ contacts/models.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 
 class Contacts(models.Model):
+	
 	listing = models.CharField(max_length=200)
 	listing_id = models.IntegerField()
 	name = models.CharField(max_length=200)
--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import Contacts
 
 # Create your views here.
 
 
 def contact(requests):
-	return 
+
+	if requests.method == 'POST':
+		listing_id = requests.POST['listing_id']
+		listing = requests.POST['listing']
+		name = requests.POST['name']
+		email = requests.POST['email']
+		phone = requests.POST['phone']
+		message = requests.POST['message']
+		user_id = requests.POST['user_id']
+		realtor_email = requests.POST['realtor_email']
+
+		contact = Contacts(listing = listing, listing_id=listing_id, name=name, email=email,phone=phone,message=message,user_id=user_id)
+		
+
+		contact.save()
+
+
+		messages.success(requests,"your request has been submitted, a realtor will get bact to you soon") 
+
+		return redirect('listings/'+listing_id)
